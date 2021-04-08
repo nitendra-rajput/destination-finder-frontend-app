b79a7204c1bd91f8ad6eacba1aab8caae38512b9
niten <nitendra.rajput@momentum.co.za>
2021-04-08 09:13:27 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LandingPageComponent} from './components/landing-page/landing-page.component';
+
+const routes: Routes = [
+  {path: '', component: LandingPageComponent, pathMatch: 'full'},
+  {path: '**', component: LandingPageComponent, pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
